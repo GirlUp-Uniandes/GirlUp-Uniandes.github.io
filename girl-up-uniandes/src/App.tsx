233fade6161b347aad5b8e42c298b9aafc01bcc1
Mc Londono <mc.londono@uniandes.edu.co>
2020-08-04 18:58:49 -0500

    configurado perfecto

--- girl-up-uniandes/src/App.tsx
+++ girl-up-uniandes/src/App.tsx
@@ -3,7 +3,12 @@ import './App.css';
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-
+import Blog from './pages/blog';
+import Causes from './pages/causes';
+import Events from './pages/events';
+import Girlupuniandes from './pages/girlupuniandes';
+import Mentorship from './pages/mentorship';
+import Whoweare from './pages/whoweare';
 import Landing from './pages/landing';
 
 
@@ -14,7 +19,12 @@ function App() {
         <div>
           <Switch>
             <Route path="/"  component={Landing} />
-            
+            <Route path="/causas" exact component={Causes} />
+            <Route path="/blog" component={Blog} />
+            <Route path="/nosotras" component={Whoweare} />
+            <Route path="/mentoria" component={Mentorship} />
+            <Route path="/girlup" component={Girlupuniandes} />
+            <Route path="/eventos" component={Events} />
             </Switch>
         </div>
       </BrowserRouter>

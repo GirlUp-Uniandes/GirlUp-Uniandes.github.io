4abe72ad1d7cba472746f7bd2afdd80edbc4c324
Mc Londono <mc.londono@uniandes.edu.co>
2020-08-04 19:39:43 -0500

    navbar

---
+++ girl-up-uniandes/src/components/navbar.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import{Nav, Navbar} from 'react-bootstrap'
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Navbar bg="primary" variant="dark">
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+            
+        </Navbar>
+    </div>
+  );
+}
--- girl-up-uniandes/src/pages/landing.tsx
+++ girl-up-uniandes/src/pages/landing.tsx
@@ -1,23 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '@material-ui/core/Button';
+import Carousel from 'react-bootstrap/Carousel'
+import Banner1 from '../images/Banner1.jpeg';
 
-export default function Landing() {
+function ControlledCarousel() {
+    const [index, setIndex] = useState(0);
+  
+    const handleSelect = (selectedIndex: React.SetStateAction<number>, e: any) => {
+      setIndex(selectedIndex);
+    };
   
     return (
+      <Carousel activeIndex={index} onSelect={handleSelect}>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={Banner1}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            
+            <p>Borre todo porque no funcionaba pero ya lo hago jaja</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        
+      </Carousel>
+    );
+  }
+export default function Landing() {
+    return (
       <div >
-        <Button variant="contained">Default</Button>
-        <Button variant="contained" color="primary">
-          Primary
-        </Button>
-        <Button variant="contained" color="secondary">
-          Secondary
-        </Button>
-        <Button variant="contained" disabled>
-          Disabled
-        </Button>
-        <Button variant="contained" color="primary" href="#contained-buttons">
-          Link
-        </Button>
+        <ControlledCarousel />
       </div>
     );
   }
